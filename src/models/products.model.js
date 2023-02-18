8a2048c7667b2afa0f8d6c693b88f4da4727a28a
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-18 00:24:23 -0300

    :sparkles: req-02

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,12 +1,13 @@
 const connection = require('./connection');
 
 const getAllProducts = async () => {
-  const [result] = await connection.execute('SELECT * FROM products');
+  const [result] = await connection.execute('SELECT * FROM StoreManager.products');
   return result;
 };
 
 const getProductById = async (id) => {
-  const [[result]] = await connection.execute('SELECT * FROM products WHERE id = ?', [id]);
+  const [[result]] = await connection
+    .execute('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
   return result;
 };
 
