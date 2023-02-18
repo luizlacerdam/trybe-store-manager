e97ed104e2b7c11e6c8d754d5eb8c231ac4c3a8f
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-17 21:14:46 -0300

    :sparkles: productsRouter-app-added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { productsRouter } = require('./routers');
 
 const app = express();
-
+app.use(express.json());
+app.use('/products', productsRouter);
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
