1b50a2d19ada7044f7632b84f782a9783ad640a0
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-17 21:05:09 -0300

    :sparkles: productsController

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,1 +1,20 @@
 const { productsService } = require('../services');
+const errorMap = require('../utils/errorMap');
+
+const getAllProducts = async (_req, res) => {
+  const { type, message } = await productsService.getAllProducts();
+  if (type) res.status(errorMap.mapError(type)).json({ message });
+  res.status(200).json(message);
+};
+
+const getProductById = async (req, res) => {
+  const { productId } = req.params;
+  const { type, message } = await productsService.getProductById(productId);
+  if (type) res.status(errorMap.mapError(type)).json({ message });
+  res.status(200).json(message);
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
