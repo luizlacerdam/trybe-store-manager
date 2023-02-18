ec4eb9b49d51951d6df259184784ee676d69a735
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-17 21:19:24 -0300

    :bug: bug-fix-in-productsServices

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,12 +1,12 @@
 const { productsModel } = require('../models');
 
 const getAllProducts = async () => {
-  const products = await productsModel.getAll();
+  const products = await productsModel.getAllProducts();
   return { type: null, message: products };
 };
 
 const getProductById = async (id) => {
-  const products = await productsModel.getById(id);
+  const products = await productsModel.getProductById(id);
   return { type: null, message: products };
 };
 
