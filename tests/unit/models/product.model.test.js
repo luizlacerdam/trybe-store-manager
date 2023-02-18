f5b430a059c2d85780bd811d5ea8b11e40241a70
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-17 23:16:22 -0300

    :sparkles: productsModelTesting

---
+++ tests/unit/models/product.model.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../src/models/connection');
+const { productsModel } = require('../../../src/models')
+const { productsList } = require('./mocks/products.model.mock')
+
+describe('1. Testes de unidade do model de products.', function () {
+  it('1.1. Recuperando todos os produtos.', async function () {
+    // Arrange
+    sinon.stub(connection, 'execute').resolves([productsList]);
+    // Act
+    const result = await productsModel.getAllProducts();
+    // Assert
+    expect(result).to.be.deep.equal(productsList);
+  })
+  it('1.2. Recuperando produto por id.', async function () {
+    const productId = 1;
+    // Arrange
+    sinon.stub(connection, 'execute').resolves([[productsList[0]]]);
+    // Act
+    const result = await productsModel.getProductById(productId);
+    // Assert
+    expect(result).to.be.deep.equal(productsList[0]);
+  })
+  afterEach(function () {
+    sinon.restore();
+  });
+})
