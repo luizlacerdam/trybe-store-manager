f28cb1800f0ef70f65db85edcd2c945ce34108bd
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-17 21:01:09 -0300

    :recycle: refatorando nome das funcoes

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,16 +1,16 @@
 const { productsModel } = require('../models');
 
-const getAll = async () => {
+const getAllProducts = async () => {
   const products = await productsModel.getAll();
   return { type: null, message: products };
 };
 
-const getById = async (id) => {
+const getProductById = async (id) => {
   const products = await productsModel.getById(id);
   return { type: null, message: products };
 };
 
 module.exports = {
-  getAll,
-  getById,
+  getAllProducts,
+  getProductById,
 };
