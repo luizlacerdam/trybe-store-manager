604d4170ebb0a43ce9595635efd5e82aad74e1f4
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-19 11:08:40 -0300

    :test_tube: bug-fixing-req-01-avaliador

--- src/server.js
+++ src/server.js
@@ -1,17 +1,8 @@
 const app = require('./app');
-const connection = require('./models/connection');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, async () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
-  // O código abaixo é para testarmos a comunicação com o MySQL
-  const [result] = await connection.execute('SELECT * FROM products');
-  if (result) {
-    console.log('MySQL connection OK');
-    console.log(result);
-  } else {
-    console.log('MySQL DOWN');
-  }
 });
