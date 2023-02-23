05b4ed7f49c98a68e850af3c94aca44d92c726bd
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-23 19:37:24 -0300

    :sparkles: validationProducts-Id-and-Schema

---
+++ src/services/validations/validationProductInputsValues.js
@@ -0,0 +1,21 @@
+const { addProductSchema, idSchema } = require('./schemas');
+
+const validateId = (id) => {
+  const { error } = idSchema.validate(id);
+  if (error) return { type: 'INVALID_VALUE', message: '"id" must be a number' };
+
+  return { type: null, message: '' };
+};
+
+const validateNewProduct = (productName) => {
+  const { error } = addProductSchema
+    .validate({ productName });
+  if (error) return { type: 'INVALID_VALUE', message: error.message };
+
+  return { type: null, message: '' };
+};
+
+module.exports = {
+  validateId,
+  validateNewProduct,
+};
