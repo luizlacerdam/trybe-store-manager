cdd1f5de3be19e030776f513865b1909809e5535
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-23 19:32:50 -0300

    :sparkles: id-and-addProducts-schemas

---
+++ src/models/sales.model.js
@@ -0,0 +1,11 @@
+// const connection = require('./connection');
+
+// const addSale = async (productName) => {
+//   const [{ insertId }] = await connection
+//     .execute('INSERT INTO StoreManager.products (name) VALUE (?)', [productName]);
+//   return insertId;
+// };
+
+// module.exports = {
+//   addSale,
+// };
