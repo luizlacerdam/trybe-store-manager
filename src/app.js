d03dc29c794468b5d5022c775cc2d430a3d59158
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-24 18:55:51 -0300

    :sparkles: getAllSales-routers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { productsRouter } = require('./routers');
+const { productsRouter, salesRouter } = require('./routers');
 
 const app = express();
 app.use(express.json());
 app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
