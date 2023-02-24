b3d39cbbbcdadab41b4bf7d12488923f330f3253
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-24 18:50:33 -0300

    :sparkles: getAllSales-controller

---
+++ src/controllers/sales.controller.js
@@ -0,0 +1,12 @@
+const { salesService } = require('../services');
+const errorMap = require('../utils/errorMap');
+
+const getAllSales = async (_req, res) => {
+  const { type, message } = await salesService.getAllSales();
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+  return res.status(200).json(message);
+};
+
+module.exports = {
+  getAllSales,
+};
