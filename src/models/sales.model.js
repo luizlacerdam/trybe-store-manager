48f1c196130468b4188356249a69eac3947ae974
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-24 18:43:57 -0300

    :sparkles: getAllSales

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,5 +1,13 @@
-// const connection = require('./connection');
-// // const getAllSales
+const connection = require('./connection');
+
+const getAllSales = async () => {
+  const [result] = await connection
+    .execute(`SELECT s.id AS saleId, p.product_id AS productId, p.quantity, s.date
+    FROM StoreManager.sales_products AS p 
+    INNER JOIN StoreManager.sales AS s ON s.id = p.sale_id
+    ORDER BY s.id, productId`);
+  return result;
+};
 // // const addSale = async (saleId, productId, quantity) => {
 // //   const placeholders = Object.keys(passenger)
 // //     .map((_key) => '?')
@@ -13,6 +21,6 @@
 // //   return insertId;
 // // };
 
-// module.exports = {
-//   // addSale,
-// };
+module.exports = {
+  getAllSales,
+};
