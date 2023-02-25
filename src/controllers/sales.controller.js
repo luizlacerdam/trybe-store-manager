f1893f0b3e1551fc80cd283533a58e3ace9d4968
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-24 23:12:49 -0300

    :sparkles: getSaleById-controller

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -7,6 +7,13 @@ const getAllSales = async (_req, res) => {
   return res.status(200).json(message);
 };
 
+const getSaleById = async (_req, res) => {
+  const { type, message } = await salesService.getSaleById();
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+  return res.status(200).json(message);
+};
+
 module.exports = {
   getAllSales,
+  getSaleById,
 };
