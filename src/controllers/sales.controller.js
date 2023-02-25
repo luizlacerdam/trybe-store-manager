3698f544ba54c962904f6c355058273c6f307b10
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-24 23:20:55 -0300

    :sparkles: req-08

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -10,6 +10,9 @@ const getAllSales = async (_req, res) => {
 const getSaleById = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await salesService.getSaleById(id);
+  if (message.length === 0 || !message) {
+    return res.status(404).json({ message: 'Sale not found' });
+  }
   if (type) return res.status(errorMap.mapError(type)).json({ message });
   return res.status(200).json(message);
 };
